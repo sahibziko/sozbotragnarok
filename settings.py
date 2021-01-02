0887ff2c28889eb5017c632721a3decd12ccbdcc
sahibziko <32236360+sahibziko@users.noreply.github.com>
2021-01-02 04:39:47 +0400

    Yenileme settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 PROXY_URL = 'https://54.37.131.45:3128'
 REQUEST_KWARGS = {'proxy_url': 'https://54.37.131.45:3128'}
-TOKEN = "1221980714:AAHuDcBOFq5ITPnG_kPQnQ76RxUfZStkJB4"
+TOKEN = "1495875069:AAF9TSdEIwHxRhozbXf44Vjq_Ib1jHlqvFY"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
