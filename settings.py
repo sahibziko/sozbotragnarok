f33a08fa9dacb811d5098f043f5e969bdc4895f3
sahibziko <32236360+sahibziko@users.noreply.github.com>
2021-01-02 04:53:30 +0400

    Dd

--- settings.py
+++ settings.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-PROXY_URL = 'https://54.37.131.45:3128'
-REQUEST_KWARGS = {'proxy_url': 'https://54.37.131.45:3128'}
+# -*- coding: utf-8 -*
 TOKEN = "1495875069:AAF9TSdEIwHxRhozbXf44Vjq_Ib1jHlqvFY"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
